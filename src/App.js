7c7ec4479be5cb3c3271eb502ded2f4625f0da76
kosmos992 <yechan5870@gmail.com>
2022-10-18 17:07:41 +0900

    add todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Todo from './components/Todo';
+
+const Cube = styled.div`
+  width: 30vw;
+  height: 30vw;
+  min-width: 400px;
+  min-height: 400px;
+  max-width: 650px;
+  max-height: 650px;
+  background-color: rgb(241, 241, 241);
+`;
+
+const Ask = styled.div`
+  font-size: xx-large;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 안녕하세요
-        </a>
-      </header>
+      <Left>
+        <p>캘린더 영역</p>
+      </Left>
+      <Center>
+        {' '}
+        <Cube />
+        <Ask>
+          <p>오늘 기분이 어떤가요?</p>
+        </Ask>
+      </Center>
+      <Right>
+        <p>투두리스트 영역</p>
+        <Todo />
+      </Right>
     </div>
   );
 }
